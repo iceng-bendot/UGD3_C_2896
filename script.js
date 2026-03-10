5aa9bcbacfec4d8709b7bddffd393c67fd969a28
iceng-bendot <vincenthadeus@gmail.com>
2026-03-10 16:13:10 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// function errorText() {
+//     let username = document.querySelector(".input-usn").value;
+//     let password = document.querySelector(".input-pw").value;
+//     let errorMessage = document.getElementsByClassName("texterror");
+
+//     if (username === "" || password === "") {
+//         errorMessage.innerText = "Username dan Password tidak boleh kosong!";
+//     } else {
+//         errorMessage.innerText = "";
+//         window.location.href("https://www.instagram.com/vincenthadeus")
+//     }
+// }
